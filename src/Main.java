294794f4353b02ead9a377757b3e16892ecb96a6
Alex <seredin-alex@mail.ru>
2022-09-26 21:05:36 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Урок 5 Задание 1");
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием");
+        }
+        int age2 = 17;
+        if (age < 18);
+        System.out.println("Возраст совершеннолетия еще не наступил. Нужно немного подождать");
+
+        System.out.println("Задание 2");
 
     }
 }
