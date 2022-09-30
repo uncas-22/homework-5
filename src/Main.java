f806221b5cf14e29f694df785e72f741eb0a7d45
Alex <seredin-alex@mail.ru>
2022-09-30 19:21:23 +0300

    Добавил задание 2 (без else)

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,21 @@ public class Main {
             System.out.println("Поздравляем с совершеннолетием");
         }
         int age2 = 17;
-        if (age < 18);
-        System.out.println("Возраст совершеннолетия еще не наступил. Нужно немного подождать");
-
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил. Нужно немного подождать");
+        }
         System.out.println("Задание 2");
-
+        int ageSchool = 7;
+        if (ageSchool >= 7) {
+            System.out.println("Ты еще ходищь в школу");
+        }
+        int ageUniversity = 18;
+        if (ageUniversity >= 18) {
+            System.out.println("Ты закончил школу. Отправляйся в университет.");
+        }
+        int ageWork = 24;
+        if (ageWork >=24) {
+            System.out.println("Ты окончил университет. Пора искать первую работу");
+        }
     }
 }
