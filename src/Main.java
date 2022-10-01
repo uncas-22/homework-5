1fb086fa2fad66607fadea42246ea8e1704f0f88
Alex <seredin-alex@mail.ru>
2022-10-01 18:12:56 +0300

    Добавил часть 3. задание 1-2-3 решение с использованием else. Дз выполнено 100%

--- src/Main.java
+++ src/Main.java
@@ -56,10 +56,59 @@ public class Main {
         } else {
             System.out.println("Сидячих мест нет");
         }
-            if (standingPlaceReserved2 < standingPlace2) {
-                System.out.println("В вагоне осталось свободно " + (standingPlace2 - standingPlaceReserved2) + " стоячих мест");
+        if (standingPlaceReserved2 < standingPlace2) {
+            System.out.println("В вагоне осталось свободно " + (standingPlace2 - standingPlaceReserved2) + " стоячих мест");
+        } else {
+            System.out.println("Стоячих мест нет");
+        }
+        System.out.println("Часть 3. Задание 1 с использованием else");
+        int age3 = 24;
+        if (age3 >= 2 && age3 < 6) {
+            System.out.println("Если возраст человека равен " + age3 + ", то ему нужно ходить в детский сад");
+        } else if (age3 >= 7 && age3 < 18) {
+            System.out.println("Если возраст человека равен " + age3 + ", то ему нужно ходить в школу");
+        } else if (age3 >= 18 && age3 < 24) {
+            System.out.println("Если возраст человека равен " + age3 + ", то ему нужно ходить в университет");
+        } else if (age3 >= 24) {
+            System.out.println("Если возраст человека равен " + age3 + ", то ему нужно ходить на работу");
+        }
+        System.out.println("Часть 3. Задание 2 с использованием else");
+        int age4 = 14;
+        if (age4 < 5) {
+            System.out.println("Ты не можешь кататься на атракционе");
+        } else if (age4 < 14) {
+            System.out.println("Ты можешь кататься на атракционе только в сопровождении взрослого");
+        } else if (age4 >= 14) {
+            System.out.println("Ты можешь кататься на атракционе без споровождения взрослого");
+        }
+        System.out.println("Часть 3. Задание 3 с использованием else");
+        int one = 7;
+        int two = 39;
+        int three = 154;
+        if (one > two) {
+            if (one > three) {
+                System.out.println("Максимальное число = " + one);
+            } else if (three > one) {
+                System.out.println("Максимальное число = " + three);
             } else {
-                System.out.println("Стоячих мест нет");
+                System.out.println("Числа " + one + " и " + three + "равны");
+            }
+        } else if (two > one) {
+            if (two > three) {
+                System.out.println("Максимальное число = " + two);
+            } else if (three > two) {
+                System.out.println("Максимальное число = " + three);
+            } else {
+                System.out.println("Числа " + two + " и " + three + "равны");
+            }
+        } else {
+            if (one > three) {
+                System.out.println("Максимальное число = " + one);
+            } else if (three > one) {
+                System.out.println("Максимальоне число = " + three);
+            } else {
+                System.out.println("Вс числа равны");
+            }
         }
-        }
+    }
 }
